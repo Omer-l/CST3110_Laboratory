3e45c09265207cf6bdc45a63f566fbcf211d038e
Omer Kacar <omerkacar@Omers-MacBook-Pro.local>
2021-11-26 20:47:28 +0000

    test(core): add TreeTest.java and Tree.java
    test coverage: 100% classes, 100% lines and 100% methods.
    Add 'add()' method with 100% branch coverage, which means
    statement coverage is also 100%.

--- src/lab6/Tree.java
+++ src/lab6/Tree.java
@@ -2,5 +2,58 @@ package lab6;
 
 public class Tree<E extends Comparable<E>> {
 
+    private MyTreeNode<E> root;
 
+    public Tree(E element) {
+        this.root = new MyTreeNode<>(element);
+    }
+
+    public Tree() {
+    }
+
+    /**
+     * traverses the tree until node to add child to is found.
+     * @param elementToAdd      the element to add to the tree
+     */
+    public void add(E elementToAdd) {
+        if(root == null)
+            this.root = new MyTreeNode<>(elementToAdd);
+        else
+            add(root, elementToAdd);
+
+    }
+
+    /**
+     * traverses the tree until node to add child to is found.
+     * @param currentNode       the node the recursion is currently at
+     * @param elementToAdd      the element to add to the tree
+     */
+    public void add(MyTreeNode<E> currentNode, E elementToAdd) {
+        E currentElement = currentNode.getElement();
+        boolean left = (currentElement.compareTo(elementToAdd) == 1 ||
+                currentElement.compareTo(elementToAdd) == 0); //<= currentNode
+
+        if(currentNode.getLeftNode() != null) {
+            MyTreeNode<E> nextNode = currentNode.getLeftNode();
+            if(left) //then traverse left if recursion left
+                add(nextNode, elementToAdd);
+        } else if(currentNode.getRightNode() != null) {
+            MyTreeNode<E> nextNode = currentNode.getRightNode();
+            if(!left)
+                add(nextNode, elementToAdd);
+
+        } else { //both branches are null and insert either
+            MyTreeNode<E> newNode = new MyTreeNode<>(elementToAdd);
+
+            if(left)
+                currentNode.setLeftNode(newNode);
+            else
+                currentNode.setRightNode(newNode);
+        }
+
+    }
+
+    public MyTreeNode<E> getRoot() {
+        return root;
+    }
 }
--- src/lab6/TreeTest.java
+++ src/lab6/TreeTest.java
@@ -1,7 +1,135 @@
 package lab6;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class TreeTest {
+public class TreeTest {
 
+    @Test
+    public void addRoot() {
+        Tree<Integer> tree = new Tree();
+        Integer rootElement = 25;
+
+        tree.add(rootElement);
+
+        Integer expected = rootElement;
+        Integer actual = tree.getRoot().getElement();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addLeft() {
+        Tree<Integer> tree = new Tree(25);
+
+        Integer leftElement = 5;
+        tree.add(leftElement);
+
+        Integer expected = leftElement;
+        Integer actual = tree.getRoot().getLeftNode().getElement();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addRight() {
+        Tree<Integer> tree = new Tree(25);
+
+        Integer rightElement = 50;
+        tree.add(rightElement);
+
+        Integer expected = rightElement;
+        Integer actual = tree.getRoot().getRightNode().getElement();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void add2Left() {
+
+        Tree<Integer> tree = new Tree(25);
+
+        Integer leftElement1 = 5;
+        tree.add(leftElement1);
+
+        Integer leftElement2 = 2;
+        tree.add(leftElement2);
+
+        Integer expected = leftElement2;
+        Integer actual = tree.getRoot().getLeftNode().getLeftNode().getElement();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void add2Right() {
+
+        Tree<Integer> tree = new Tree(25);
+
+        Integer rightElement1 = 50;
+        tree.add(rightElement1);
+
+        Integer rightElement2 = 250;
+        tree.add(rightElement2);
+
+        Integer expected = rightElement2;
+        Integer actual = tree.getRoot().getRightNode().getRightNode().getElement();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addLeftRight() {
+
+        Tree<Integer> tree = new Tree(25);
+
+        Integer leftElement = 10;
+        tree.add(leftElement);
+
+        Integer leftRightElement = 15;
+        tree.add(leftRightElement);
+
+        Integer expected = leftRightElement;
+        Integer actual = tree.getRoot().getLeftNode().getRightNode().getElement();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addRightLeft() {
+
+        Tree<Integer> tree = new Tree(25);
+
+        Integer rightElement = 50;
+        tree.add(rightElement);
+
+        Integer rightLeftElement = 35;
+        tree.add(rightLeftElement);
+
+        Integer expected = rightLeftElement;
+        Integer actual = tree.getRoot().getRightNode().getLeftNode().getElement();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addLeftLeftRight() {
+
+        Tree<Integer> tree = new Tree(25);
+
+        Integer leftElement = 20;
+        tree.add(leftElement);
+
+        Integer leftLeftElement = 10;
+        tree.add(leftLeftElement);
+
+        Integer leftLeftRight = 15;
+        tree.add(leftLeftRight);
+
+        Integer expected = leftLeftRight;
+        Integer actual = tree.getRoot().getLeftNode().getLeftNode().getRightNode().getElement();
+
+        assertEquals(expected, actual);
+    }
 }
