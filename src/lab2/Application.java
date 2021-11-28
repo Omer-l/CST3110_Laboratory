941d7cca952105bcd7000a24488294be792044d0
= <=>
2021-11-28 22:55:40 +0000

    refactor(core): ad Hourglass.java class to lab2
    This will make it easier to test the hourglass algorithm.

--- src/lab2/Application.java
+++ src/lab2/Application.java
@@ -2,8 +2,6 @@ package lab2;
 
 public class Application {
 
-    private static int[] indexOfMaximumHourGlass = new int[2];
-
     public static void main(String[] args) {
         int[][] matrix = {
                 {-9, -9, -9, 1, 1, 1},
@@ -14,64 +12,13 @@ public class Application {
                 {0, 0, 1, 2, 4, 0}
         };
 
-        printMatrix(matrix);
+        Hourglass hourglass = new Hourglass(matrix);
 
-        int maximumHourGlass = hourGlass(matrix);
-        int rowOfHourGlass = indexOfMaximumHourGlass[0];
-        int columnOfHourGlass = indexOfMaximumHourGlass[1];
-
-        System.out.println("Maximum hourglass has a sum of: " + maximumHourGlass +
-                "\nCentre of hourglass at: row " + rowOfHourGlass + " column: " + columnOfHourGlass);
-    }
-
-    /**
-     * iteratively evaluates the matrix, moving the hourglass sideways moving along row by row.
-     * @param matrix    an nxn matrix that be evaluated for the maximum hourglass sum.
-     * @return          hourglass with the highest sum
-     */
-    public static int hourGlass(int[][] matrix) {
-        int maximumHourGlassSum = getHourGlassSum(matrix, 1, 1);
-
-        for (int rowIterator = 1; rowIterator < matrix.length - 1; rowIterator++)  //loop from 2nd row to 2nd to last row.
-            for (int columnIterator = 1; columnIterator < matrix[rowIterator].length - 1; columnIterator++) { //from 2nd column to 2nd to last column.
-                //create a temporary sum to compare with current maximumHourGlassSum
-                int tmpSum = getHourGlassSum(matrix, rowIterator, columnIterator);
-
-                if (tmpSum > maximumHourGlassSum) {
-                    indexOfMaximumHourGlass[0] = rowIterator;
-                    indexOfMaximumHourGlass[1] = columnIterator;
-                    maximumHourGlassSum = tmpSum; //change current maximumHourGlassSum.
-                }
-            }
-
-        return maximumHourGlassSum;
-    }
-
-    /**
-     * calculates the sum of the hourglass, given the centre point of the hour glass.
-     * @param matrix    nxn matrix to be evaluated
-     * @param row         the row number of the matrix
-     * @param column         the column number of the matrix
-     * @return          sum of the values in the hourglass.
-     */
-    public static int getHourGlassSum(int[][] matrix, int row, int column) {
-        int tmpSum = 0;
-
-        tmpSum += matrix[row - 1][column - 1];
-        tmpSum += matrix[row - 1][column];
-        tmpSum += matrix[row - 1][column + 1]; //top of hour glass
-
-        tmpSum += matrix[row][column]; //centre
-
-        tmpSum += matrix[row + 1][column - 1];
-        tmpSum += matrix[row + 1][column];
-        tmpSum += matrix[row + 1][column + 1]; //bottom of hour glass
-
-        return tmpSum;
+        hourglass.calculateAndPrintMaximumHourglass();
     }
 
     //Prints the 2D array representing a matrix.
-    private static void printMatrix(int[][] matrix) {
+    public static void printMatrix(int[][] matrix) {
         for (int rowIterator = 0; rowIterator < matrix.length; rowIterator++) {  //loop row
             for (int columnIterator = 0; columnIterator < matrix[rowIterator].length; columnIterator++) //loop column
                 System.out.print(matrix[rowIterator][columnIterator] + "\t");
---
+++ src/lab2/Hourglass.java
@@ -0,0 +1,69 @@
+package lab2;
+
+public class Hourglass {
+
+    private int[] indexOfMaximumHourGlass;
+    private int[][] matrix;
+
+    public Hourglass(int[][] matrix) {
+        this.matrix = matrix;
+        this.indexOfMaximumHourGlass = new int[2];
+    }
+
+    public void calculateAndPrintMaximumHourglass() {
+        int maximumHourGlass = hourGlass(matrix);
+        int rowOfHourGlass = indexOfMaximumHourGlass[0];
+        int columnOfHourGlass = indexOfMaximumHourGlass[1];
+
+        Application.printMatrix(matrix);
+
+        System.out.println("Maximum hourglass has a sum of: " + maximumHourGlass +
+                "\nCentre of hourglass at: row " + rowOfHourGlass + " column: " + columnOfHourGlass);
+    }
+
+    /**
+     * iteratively evaluates the matrix, moving the hourglass sideways moving along row by row.
+     * @param matrix    an nxn matrix that be evaluated for the maximum hourglass sum.
+     * @return          hourglass with the highest sum
+     */
+    public int hourGlass(int[][] matrix) {
+        int maximumHourGlassSum = getHourGlassSum(matrix, 1, 1);
+
+        for (int rowIterator = 1; rowIterator < matrix.length - 1; rowIterator++)  //loop from 2nd row to 2nd to last row.
+            for (int columnIterator = 1; columnIterator < matrix[rowIterator].length - 1; columnIterator++) { //from 2nd column to 2nd to last column.
+                //create a temporary sum to compare with current maximumHourGlassSum
+                int tmpSum = getHourGlassSum(matrix, rowIterator, columnIterator);
+
+                if (tmpSum > maximumHourGlassSum) {
+                    indexOfMaximumHourGlass[0] = rowIterator;
+                    indexOfMaximumHourGlass[1] = columnIterator;
+                    maximumHourGlassSum = tmpSum; //change current maximumHourGlassSum.
+                }
+            }
+
+        return maximumHourGlassSum;
+    }
+
+    /**
+     * calculates the sum of the hourglass, given the centre point of the hour glass.
+     * @param matrix    nxn matrix to be evaluated
+     * @param row         the row number of the matrix
+     * @param column         the column number of the matrix
+     * @return          sum of the values in the hourglass.
+     */
+    public int getHourGlassSum(int[][] matrix, int row, int column) {
+        int tmpSum = 0;
+
+        tmpSum += matrix[row - 1][column - 1];
+        tmpSum += matrix[row - 1][column];
+        tmpSum += matrix[row - 1][column + 1]; //top of hour glass
+
+        tmpSum += matrix[row][column]; //centre
+
+        tmpSum += matrix[row + 1][column - 1];
+        tmpSum += matrix[row + 1][column];
+        tmpSum += matrix[row + 1][column + 1]; //bottom of hour glass
+
+        return tmpSum;
+    }
+}
