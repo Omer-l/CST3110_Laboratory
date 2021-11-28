6ee78c9da6336b446034b442fc16b6b853dca1c7
Omer Kacar <omerkacar@Omers-MacBook-Pro.local>
2021-11-28 00:09:03 +0000

    test(core): add test functions for ApplicationRotateArray.java
    Also, add coverage results to README.md. The method coverage is not
    100% because the remaining percentage is the 'main(String[] args)'
    method.

--- src/lab1/ApplicationRotateArray.java
+++ src/lab1/ApplicationRotateArray.java
@@ -3,15 +3,8 @@ package lab1;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Application {
-    /*
-    *
+public class ApplicationRotateArray {
 
-    Given an array of n integers and a number d, write a Java method that
-    performs d left rotations on the array. Then print the updated array as a
-    single line of space-separated integers.
-
-     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Integer[] arrayToRotate = {1, 2, 3, 4, 5};
@@ -26,9 +19,9 @@ public class Application {
 
     /**
      * Shifts elements to the left until given number of rotations.
-     * @param arrayToRotate       array to shift
-     * @param numberOfRotations         index to shift elements to the left by
-     * @param <E>       type of array (Integer, String etc)
+     * @param arrayToRotate         array to shift
+     * @param numberOfRotations    index to shift elements to the left by
+     * @param <E>                   type of array (Integer, String etc)
      */
     public static<E> void leftRotate(E[] arrayToRotate, int numberOfRotations) {
         E[] temp = arrayToRotate.clone();
--- src/lab1/ApplicationRotateArrayTest.java
+++ src/lab1/ApplicationRotateArrayTest.java
@@ -7,6 +7,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class ApplicationRotateArrayTest {
 
     @Test
-    void leftRotate() {
+    void twoRotations() {
+        Integer[] arrayToRotate = {1, 2, 3, 4, 5};
+        int numberOfRotations = 2;
+
+        ApplicationRotateArray.leftRotate(arrayToRotate, numberOfRotations);
+
+        Integer[] actuals = arrayToRotate;
+        Integer[] expecteds = {3, 4, 5, 1, 2};
+
+        assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    void fiveRotations() {
+        Integer[] arrayToRotate = {1, 2, 3, 4, 5};
+        int numberOfRotations = 5;
+
+        ApplicationRotateArray.leftRotate(arrayToRotate, numberOfRotations);
+
+        Integer[] actuals = arrayToRotate;
+        Integer[] expecteds = {1, 2, 3, 4, 5};
+
+        assertArrayEquals(expecteds, actuals);
     }
 }
