cf09ee6f3d5040b7f7dea933283a7a78c3f6a9f6
Omer Kacar <ok243@live.mdx.ac.uk>
2021-11-29 23:32:32 +0000

    test(core): add MyNodeLLTest.java and MyLinkedListTest.java
    Node test class is complete and covers the necessary functions and
    equivalence partitions. The LinkedListTest.java contains equivalence
    partition tests for adding 1 element (front of linked list) and adding
    2 elements (front and back of list) and lastly, adding 3 elements tested
    and passed. Linked list tests cover all the necessary functions in the
    tests including branch coverage. Hence, coverage so far is 100%, only
    thing not covered are the getters and setters which are not very
    important as of yet.

--- src/lab3/MyLinkedList.java
+++ src/lab3/MyLinkedList.java
@@ -1,11 +1,72 @@
 package lab3;
 
 public class MyLinkedList<E> {
-    private int index = 0;
-    private int size = 1;
-//    private E[] elements = new int[size];
+    private int size = 0;
+    private MyNodeLL<E> first;
+    private MyNodeLL<E> last;
 
-//    public MyLinkedList<E>(E element) {
-//        add(element);
-//    }
+    public MyLinkedList(E element) {
+        this.first = new MyNodeLL<>(element);
+    }
+
+    public MyLinkedList() {
+    }
+
+    //returns true if linked list is empty.
+    public boolean empty() {
+        return size == 0;
+    }
+    /**
+     * Adds element as first if there are no nodes in list, otherwise
+     * add to the end of the linkedlist.
+     * @param element       element to add to the linked list
+     */
+    public void add(E element) {
+        MyNodeLL<E> newNode = new MyNodeLL<>(element);
+
+        if(empty())
+            first = newNode;
+        else
+            add(newNode);
+
+        size++;
+    }
+
+    //adds new node to the end of the linked list.
+    public void add(MyNodeLL<E> node) {
+        if(last == null) {
+            last = node;
+            last.setPrevious(first);
+            first.setNext(last);
+        } else {
+            MyNodeLL<E> tmp = last;
+            last = node;
+            node.setPrevious(tmp);
+            tmp.setNext(node);
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public MyNodeLL<E> getFirst() {
+        return first;
+    }
+
+    public void setFirst(MyNodeLL<E> first) {
+        this.first = first;
+    }
+
+    public MyNodeLL<E> getLast() {
+        return last;
+    }
+
+    public void setLast(MyNodeLL<E> last) {
+        this.last = last;
+    }
 }
--- src/lab3/MyLinkedListTest.java
+++ src/lab3/MyLinkedListTest.java
@@ -1,15 +1,43 @@
 package lab3;
 
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyLinkedListTest {
 
-//    @Test
-//    public void testAddWhenSize0() {
-//        MyLinkedList<Integer> l = new MyLinkedList<>();
-//        l.add(2);
-//        assertEquals((Integer)2, l.getFirst());
-//    }
+    @Test
+    public void testAdd1() {
+        MyLinkedList<Integer> l = new MyLinkedList<>();
+        l.add(2);
+
+        Integer expected = 2;
+        Integer actual = l.getFirst().getElement();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAdd2() {
+        MyLinkedList<Integer> l = new MyLinkedList<>();
+        l.add(2);
+        l.add(3);
+
+        Integer[] expecteds = {2, 3};
+        Integer[] actuals = {l.getFirst().getElement(), l.getFirst().getNext().getElement()};
+        assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    public void testAdd3() {
+        MyLinkedList<Integer> l = new MyLinkedList<>();
+        l.add(2);
+        l.add(3);
+        l.add(4);
+
+        Integer[] expecteds = {2, 3, 4};
+        Integer[] actuals = {l.getFirst().getElement(), l.getFirst().getNext().getElement(), l.getFirst().getNext().getNext().getElement()};
+        assertArrayEquals(expecteds, actuals);
+    }
+
 //
 //    @Test
 //    public void testGetLast() {
